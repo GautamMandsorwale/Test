3a79ac54fee89642c68cdec65352ce28a900fdc8
Gautam Mandsorwale <Gautam@Gautams-MacBook-Pro.local>
2015-07-01 02:22:28 +0530

    Implemented Popular Movie Details Feature for Phone.

--- app/src/main/java/com/nanodegree/projectzero/popularmovies/constants/NetworkConstants.java
+++ app/src/main/java/com/nanodegree/projectzero/popularmovies/constants/NetworkConstants.java
@@ -5,7 +5,7 @@ package com.nanodegree.projectzero.popularmovies.constants;
  */
 public class NetworkConstants {
 
-    public static final String MOVIES_DB_API_KEY = "469d8d0e367b1475f4debb4164b94c90";
+    public static final String MOVIES_DB_API_KEY = "";
     public static final String MOVIES_HTTP_URL_SORT_POPULARITY_DESC = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=";
     public static final String MOVIES_HTTP_URL_SORT_HIGHEST_RATED_DESC = "http://api.themoviedb.org/3/discover/movie?sort_by=vote_count.desc&api_key=";
     public static final String MOVIE_POSTER_IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
